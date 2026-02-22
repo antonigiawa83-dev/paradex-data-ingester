d4f4598b650ac0999a4f8d971d5bf96622c3f4a3
antonigiawa83-dev <antonigiawa83@gmail.com>
2026-02-22 20:13:58 +0700

    Disable RunConsumer function temporarily
    Temporarily disable RunConsumer function to avoid conflicts.

--- consumer.go
+++ consumer.go
@@ -1,1 +1,9 @@
 
+package main
+
+import "fmt"
+
+// RunConsumer saat ini dinonaktifkan sementara agar tidak konflik dengan chart engine
+func RunConsumer() {
+	fmt.Println(" [INFO] Consumer worker is on standby.")
+}
