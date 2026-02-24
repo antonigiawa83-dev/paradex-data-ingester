bc3c098991d4bccd62c55f82ea371b34a11135fe
antonigiawa83-dev <antonigiawa83@gmail.com>
2026-02-24 19:51:35 +0700

    Implement StartConsumer to simulate price data fetching
    Replace RunConsumer with StartConsumer to simulate data retrieval.

--- consumer.go
+++ consumer.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"time"
+)
 
-func RunConsumer() {
-	fmt.Println(" [INFO] Consumer worker siap menerima data.")
+func StartConsumer() {
+	for {
+		// Simulasi pengambilan data harga
+		price := 67000.0 + float64(time.Now().Second())
+		timestamp := time.Now().Unix()
+		
+		// Simpan ke storage.go
+		SaveTrade(price, timestamp)
+		
+		time.Sleep(2 * time.Second)
+	}
 }
