cd9ac8be5e78732610dba406a67d26583ce572a7
antonigiawa83-dev <antonigiawa83@gmail.com>
2026-02-24 20:08:38 +0700

    Update comments for clarity in StartConsumer function

--- consumer.go
+++ consumer.go
@@ -6,11 +6,11 @@ import (
 
 func StartConsumer() {
 	for {
-		// Simulasi pengambilan data harga
+		// Simulasi harga sekitar 67000 seperti di terminal kamu
 		price := 67000.0 + float64(time.Now().Second())
 		timestamp := time.Now().Unix()
 		
-		// Simpan ke storage.go
+		// Kirim data ke storage.go
 		SaveTrade(price, timestamp)
 		
 		time.Sleep(2 * time.Second)
